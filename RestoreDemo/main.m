f6802bc054be5032e44062420abf4b90b9bc1da8
cerastes <chn_jin@163.com>
2016-03-23 14:16:50 +0800

    Initial Commit

---
+++ RestoreDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RestoreDemo
+//
+//  Created by JOB on 16/3/23.
+//  Copyright © 2016年 mobi.wonders.ios.apps.cswhy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
